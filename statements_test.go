6ddfc654f4b8adfb3e39dacf494a70750b9e75da
Philip O'Toole <philip.otoole@yahoo.com>
2025-03-18 09:13:13 -0400

    Unit test statements

---
+++ statements_test.go
@@ -0,0 +1,54 @@
+package http
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_NewSQLStatementFrom_Positional(t *testing.T) {
+	for i, tt := range []struct {
+		stmt string
+		args []any
+		want *SQLStatement
+	}{
+		{
+			stmt: "SELECT * FROM foo",
+			want: &SQLStatement{SQL: "SELECT * FROM foo"},
+		},
+		{
+			stmt: "SELECT * FROM foo WHERE id = ?",
+			args: []any{42},
+			want: &SQLStatement{SQL: "SELECT * FROM foo WHERE id = ?", PositionalParams: []any{42}},
+		},
+		{
+			stmt: "SELECT * FROM foo WHERE id = ? AND name = ?",
+			args: []any{42, "hello"},
+			want: &SQLStatement{SQL: "SELECT * FROM foo WHERE id = ? AND name = ?", PositionalParams: []any{42, "hello"}},
+		},
+	} {
+		got, err := NewSQLStatementFrom(tt.stmt, tt.args...)
+		if err != nil {
+			t.Fatalf("[%d] %v", i, err)
+		}
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Fatalf("[%d] got: %v, want: %v", i, got, tt.want)
+		}
+	}
+}
+
+func Test_NewSQLStatementFrom_Named(t *testing.T) {
+	got, err := NewSQLStatementFrom("SELECT * FROM foo WHERE id = :id AND name = :name", map[string]any{
+		"id":   42,
+		"name": "hello",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := &SQLStatement{SQL: "SELECT * FROM foo WHERE id = :id AND name = :name", NamedParams: map[string]any{
+		"id":   42,
+		"name": "hello",
+	}}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got: %v, want: %v", got, want)
+	}
+}
